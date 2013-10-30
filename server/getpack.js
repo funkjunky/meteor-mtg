f950f6fc63c36f23c3b325c4aed0d6a6831deebb
Jason McCarrell <jason.abz@gmail.com>
2013-10-29 21:46:21 -0400

    Feature #227292: foils now have a purple background, als i put the odds back to 1:65 of getting a foil

--- server/getpack.js
+++ server/getpack.js
@@ -14,7 +14,7 @@ getpack = function(set, random)
 	var set = Sets.findOne({name: set}).cards;
 
 	//roll to seeif this pack contains a foil
-	var has_foil = rand_int(3) == 0; //1:65
+	var has_foil = rand_int(65) == 0; //1:65
 	var has_mythic = rand_int(8) == 0; //1:8
 
 	//change the rare slot to a mythic slot
